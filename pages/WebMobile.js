0147407be4c9b3522f2098ccefc03287766131fa
Hyerin <hyerin.bcit@gmail.com>
2022-01-17 11:09:37 -0800

    fix routing

--- pages/WebMobile.js
+++ pages/WebMobile.js
@@ -112,7 +112,9 @@ export default function WebMobile() {
           projDesc="Book Cafe Website"
         />
 
-        <Projects />
+        <Projects
+          routeTo='/Wecycle'
+        />
 
         <Projects
           routeTo='/Natiive'
