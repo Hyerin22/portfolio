29737edc958bce6535b7687ecc2e8bcc46a52744
Hyerin <hyerin.bcit@gmail.com>
2022-01-28 16:54:41 -0800

    page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+  <Component {...pageProps} />
+  <Head>
+    <title>Hyerin Cheon</title>
+  </Head>
+    
+    </div>
+
 }
 
 export default MyApp
